c420eebf5aa29dc2c88a7696760be63fbf89b0ad
lprmrl <blablatest@gmail.com>
2022-06-21 12:19:49 +0300

    Тестирование и тестовые-среды (стенды)

---
+++ provision.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt -y ubgrade
+
+sudo mkdir /vagrant
+
+sudo apt install -y python3
+sudo apt install -y python3-pip
+sudo apt install -y python3-psycopg2
+sudo apt install -y python3-django
+#sudo mkdir /vagrant
